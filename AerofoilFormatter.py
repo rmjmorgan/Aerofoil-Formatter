578c0e6197f32f101d73726ab63c865f41d4a0ad
Rhys Morgan <rhysmjm@gmail.com>
2020-10-19 13:27:12 +0100

    Major fixes.

--- AerofoilFormatter.py
+++ AerofoilFormatter.py
@@ -7,12 +7,13 @@ import os
 os.system('cls')
 
 # Help section.
-print("v1.4.3.0")
+print("v1.5.1.0")
 print("\nThis is a tool for converting 4-digit NACA aerofoil coordinates from airfoiltools.com.\n\
-Make sure that the shape does not have a closed trailing edge.\n\
+Make sure that the shape has a closed trailing edge.\n\
 Copy everything from the text box on that website (including the information on the first line)\n\
 and paste into a plain-text file before using this program.\n\n\
-Drag and drop that file here. Alternatively, enter the file path manually.\n")
+Drag and drop that file here. Alternatively, enter the file path manually.\n\
+The output will not be a closed shape. You must do that yourself in ANSYS.\n")
 
 # Define path to coordinates file.
 path = input("> Enter file path: ")
@@ -40,6 +41,10 @@ print("\nProcess complete.\n")
 x_coords = [x.replace(" ","")[:8] for x in content]
 y_coords = [y.split(" ")[-1] for y in content]
 
+# Remove final coordinates as it is a duplicate of the first element.
+del x_coords[-1]
+del y_coords[-1]
+
 # Write to file.
 name = input("> Save as: ")
 name = name + ".txt"
@@ -51,7 +56,6 @@ with open(name, 'w') as f:
     f.write("#Group" + " "*3 + "Point" + " "*8 + "X" + " "*15 + "Y" + " "*15 + "Z\n")
     for i in range(len(x_coords)):
         f.write("1        " + str(i+1) + " "*8 + x_coords[i] + " "*8 + y_coords[i] + "        0.000000\n")
-    f.write("1        0")
 print("Done.\n")
 
 close = input("Press \"enter\" to close the program.")
